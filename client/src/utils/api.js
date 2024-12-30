1cb85fd34ae35d1b75fe2a8d6c3fbfd100c946c0
Mohammed Taysir Ali <118166509+alitaysir@users.noreply.github.com>
2024-12-31 02:21:38 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5001/api/test',  // Replace with your backend API
+  baseURL: 'https://form-editor-ssql.onrender.com/api/test',  // Replace with your backend API
   timeout: 1000,
 });
